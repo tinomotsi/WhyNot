e89a3b6b47bb437dd4e5c690c4d394c1bd6ab441
Tino Tino Tino <tino@whynot.com>
2021-06-21 22:43:11 +0200

    The important tings

---
+++ Oware/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
